227c06cec9410002b440bf71c11d89a1ef39b073
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2023-07-04 23:42:12 +0530

    Cleaned data_model.dart

--- Jal in One Project Code/lib/models/data_model.dart
+++ Jal in One Project Code/lib/models/data_model.dart
@@ -1,30 +1,30 @@
+// Defining a class called "Ward"
 class Ward {
-  final String wardNo;
-  final String ownerName;
-  final String tapNo;
-  final double billDue;
-  final String noOfTaps;
-  final String seqNo;
-  final String tapSize;
-  final String tapType;
-  final String tapUsage;
-  final String userId;
+  // Declaring instance variables
+  final String wardNo; // The ward number
+  final String ownerName; // The name of the owner
+  final String tapNo; // The tap number
+  final double billDue; // The amount of bill due
+  final String noOfTaps; // The number of taps
+  final String seqNo; // The sequence number
+  final String tapSize; // The size of the tap
+  final String tapType; // The type of the tap
+  final String tapUsage; // The usage of the tap
+  final String userId; // The user ID
 
-
-
+  // Constructor for the Ward class
   Ward(
     {
-      required this.wardNo,
-      required this.userId,
-      required this.ownerName,
-      required this.tapNo,
-      required this.tapUsage,
-      required this.tapType,
-      required this.tapSize,
-      required this.seqNo,
-      required this.noOfTaps,
-      required this.billDue,
+    required this.wardNo, // Initializing the ward number
+    required this.userId, // Initializing the user ID
+    required this.ownerName, // Initializing the owner name
+    required this.tapNo, // Initializing the tap number
+    required this.tapUsage, // Initializing the tap usage
+    required this.tapType, // Initializing the tap type
+    required this.tapSize, // Initializing the tap size
+    required this.seqNo, // Initializing the sequence number
+    required this.noOfTaps, // Initializing the number of taps
+    required this.billDue, // Initializing the bill due
     }
   );
 }
-
