23c44b1ff4facb283d7194a26bfc2f3b1cbeb8da
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2023-07-05 23:59:07 +0530

    Cleaned ward_model.dart

--- Jal in One Project Code/lib/models/ward_model.dart
+++ Jal in One Project Code/lib/models/ward_model.dart
@@ -1,18 +1,20 @@
+// Defining a class called "WardData"
 class WardData {
-  final String wardNo;
-  final String officerContact;
-  final String officerName;
-  final String waterSupplyDate;
-  final String wardId;
+  // Declaring instance variables
+  final String wardNo; // The ward number
+  final String officerContact; // The contact information of the officer
+  final String officerName; // The name of the officer
+  final String waterSupplyDate; // The date of water supply
+  final String wardId; // The ID of the ward
 
-
+  // Constructor for the WardData class
   WardData(
-        {
-            required this.wardNo,
-            required this.officerContact,
-            required this.officerName,
-            required this.waterSupplyDate,
-            required this.wardId,
-        }
-      );
+    {
+      required this.wardNo, // Initializing the ward number
+      required this.officerContact, // Initializing the officer's contact information
+      required this.officerName, // Initializing the officer's name
+      required this.waterSupplyDate, // Initializing the date of water supply
+      required this.wardId, // Initializing the ward ID
+    }
+  );
 }
