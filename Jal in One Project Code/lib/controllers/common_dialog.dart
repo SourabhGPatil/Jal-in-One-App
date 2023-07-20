d6b22a7c23c3df860a44d834351a0d0f685cf7a7
Sourabh Patil <sourabh@techie.com>
2023-07-20 22:34:40 +0530

    Added Desc to CommonDialog Class

--- Jal in One Project Code/lib/controllers/common_dialog.dart
+++ Jal in One Project Code/lib/controllers/common_dialog.dart
@@ -2,7 +2,9 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
+// Defining a class called CommonDialog
 class CommonDialog {
+ // Function to show a loading dialog with an optional title 
  static showLoading({String title = "Loading..."}) {
     Get.dialog(
       Dialog(
@@ -36,10 +38,12 @@ class CommonDialog {
     );
   }
 
+ // Function to hide the loading dialog
  static hideLoading() {
     Get.back();
   }
 
+ // Function to show an error dialog with optional title and description
  static showErrorDialog(
       {String title = "Oops Error",
       String description = "Something went wrong "}) {
@@ -76,6 +80,19 @@ class CommonDialog {
       barrierDismissible: false,
     );
   }
-
-
 }
+
+/**
+The code defines a class called CommonDialog, which provides utility functions for displaying loading and error dialogs using the GetX package.
+
+1. The showLoading function displays a loading dialog with an optional title.
+It shows a circular progress indicator and the specified title, indicating that the app is performing some task or fetching data.
+The loading dialog is modal, meaning it blocks user interaction with the rest of the app until it is dismissed.
+
+2. The hideLoading function dismisses the currently displayed dialog, which is typically used to hide the loading dialog when the loading process is completed.
+
+3. The showErrorDialog function displays an error dialog with an optional title and description.
+It is used to inform the user about any errors or issues that occurred during a particular operation.
+The error dialog shows the specified title, description, and an "Okay" button for the user to acknowledge the error.
+Like the loading dialog, the error dialog is also modal, preventing user interaction with the rest of the app until dismissed. 
+*/
