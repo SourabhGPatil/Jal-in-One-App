ebeba7b9ea5546618cf32cebd97436738578d2ac
Sourabh Patil <sourabh@techie.com>
2023-07-22 22:28:46 +0530

    Added Desc. to DataController class

--- Jal in One Project Code/lib/controllers/data_controller.dart
+++ Jal in One Project Code/lib/controllers/data_controller.dart
@@ -7,30 +7,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/ward_model.dart';
 import '../models/data_model.dart';
 
+// Defining a class called DataController
 class DataController extends GetxController{
 
 
   //Created an instance of the cloud firestore
   final firebaseInstance = FirebaseFirestore.instance;
 
-  //Created a map where 2 keys are passed
+  // Created two maps to store ward profile data and user profile data
   Map wardProfileData = {'wa_no': '', 'ward_officer_contact': '', 'ward_officer_name': '', 'ward_supply_date': ''};
   Map userProfileData = {'bill_due': '', 'no_of_taps': '', 'owner_name': '', 'seq_no': '', 'tap_no': '', 'tap_size': '', 'tap_type': '', 'tap_usage': '',};
-
-
 
   //Instance of AuthController
   AuthController authController = AuthController();
 
-
-
-  //--List to handle all the User data from DB--//
+  // List to handle all the User data from the database
   List<Ward> loginUserData = [];
 
-  //--List to handle all the Ward data from DB--//
+  // List to handle all the Ward data from the database
   List<WardData> wardData = [];
 
-  //Whenever this controller file is ready, call the given methods
+  // This method is called when the controller is ready to be used
   void onReady() {
     super.onReady();
     getUserProfileData();
@@ -96,7 +93,7 @@ class DataController extends GetxController{
 
 
 
-  // //Method (function) for getting the User Profile Data from the Firestore DB
+  // Method for getting the User Profile Data from the Firestore DB
   Future<void> getUserProfileData() async {
     // print("user id ${authController.userId}");
     try {
@@ -106,7 +103,7 @@ class DataController extends GetxController{
       //--Query Ends--//
 
 
-
+      // Querying the Firestore to get the user's data based on the tap number
       var response2 = await firebaseInstance.collection('Users').where('tap_no', isEqualTo: AuthController.tap_no).get();
 
 
@@ -126,6 +123,7 @@ class DataController extends GetxController{
       // }
 
       if (response2.docs.length > 0) {
+        // Saving the retrieved user profile data into the userProfileData map
         userProfileData['bill_due'] = response2.docs[0]['bill_due'];
         userProfileData['no_of_taps'] = response2.docs[0]['no_of_taps'];
         userProfileData['owner_name'] = response2.docs[0]['owner_name'];
@@ -135,9 +133,6 @@ class DataController extends GetxController{
         userProfileData['tap_usage'] = response2.docs[0]['tap_usage'];
         userProfileData['tap_type'] = response2.docs[0]['tap_type'];
       }
-
-
-
 
 
       //print(wardProfileData);
@@ -152,10 +147,7 @@ class DataController extends GetxController{
 
 
 
-
-
-
-  //---2. To get User Data---//
+  //---2. Method for getting the User Data from the Firestore DB---//
   Future<void> getUserData() async {
     print("loginUserData YEs $loginUserData");
     print("Random");
@@ -165,7 +157,7 @@ class DataController extends GetxController{
     print("zzzz");
     print(AuthController.tap_no);
 
-    //--empty the data from the loginUserData list--//
+    // Emptying the data from the loginUserData list
     loginUserData = [];
     var tap = AuthController.tap_no;
 
@@ -181,13 +173,14 @@ class DataController extends GetxController{
 
 
       if (userResponse.docs.length > 0) {
-        //loop. The data of indexing is stored in "result"
+        //Loop through the retrieved data and add it to the loadedProduct list.
+        //The data of indexing is stored in "result"
         userResponse.docs.forEach(
               (result) {
             print(result.data());
             print("User Firebase Document ID :  ${result.id}");
 
-            //add Data into the list
+            //Add Data into the list
             lodadedProduct.add(
               Ward(
                 userId: result.id,
@@ -205,7 +198,7 @@ class DataController extends GetxController{
           },
         );
       }
-      //--added data in the "loginUserData" list--//
+      // Adding data in the "loginUserData" list
       loginUserData.addAll(lodadedProduct);
       update();
       CommonDialog.hideLoading();
@@ -220,7 +213,7 @@ class DataController extends GetxController{
   }
 
 
-  //--3. Fetch data from Ward DB to show on UI--//
+  //--3. // Method for fetching data from the Ward DB to show on UI --//
 
 
   Future<void> getWardData() async {
@@ -232,7 +225,7 @@ class DataController extends GetxController{
     print("zzzz");
     print(AuthController.tap_no);
 
-    //--empty the data from the wardData list--//
+    // Emptying the data from the wardData list
     wardData = [];
 
     try {
@@ -247,13 +240,14 @@ class DataController extends GetxController{
 
 
       if (userResponse.docs.length > 0) {
-        //loop. The data of indexing is stored in "result2"
+        //Loop through the retrieved data and add it to the loadedProduct2 list
+        //The data of indexing is stored in "result2"
         userResponse.docs.forEach(
               (result2) {
             print(result2.data());
             print("Ward Firebase Document ID :  ${result2.id}");
 
-            //add Data into the list
+            //Add Data into the list
             lodadedProduct2.add(
               WardData(
                 wardId: result2.id,
@@ -266,7 +260,8 @@ class DataController extends GetxController{
           },
         );
       }
-      //--added data in the "wardData" list--//
+
+      // Adding data in the "wardData" list
       wardData.addAll(lodadedProduct2);
       update();
       CommonDialog.hideLoading();
@@ -280,3 +275,31 @@ class DataController extends GetxController{
     }
   }
 }
+
+/**
+This Dart code defines a class called DataController, which is responsible for managing and retrieving data from Firebase Firestore for the user profile and ward details. Here's a breakdown of what the code does:
+
+1. Import necessary dependencies and packages for the class.
+
+2. Define a class called DataController that extends GetxController, which means it can be used with the GetX state management library.
+
+3. Create an instance of the Firebase Firestore.
+
+4. Declare two maps named wardProfileData and userProfileData to store ward profile data and user profile data, respectively.
+
+5. Create an instance of the AuthController, which seems to handle authentication-related tasks.
+
+6. Define two lists, loginUserData and wardData, to hold user and ward data from the database, respectively.
+
+7. Implement the onReady() method, which is a GetX lifecycle method that runs when the controller is ready. It calls the methods getUserProfileData() and getUserData().
+
+8. getUserProfileData(): This method queries the Firestore database to get the user's profile data based on the AuthController's tap_no. The retrieved data is saved into the userProfileData map.
+
+9. getUserData(): This method queries the Firestore database to get user data based on the AuthController's tap_no. It saves the data into the loginUserData list, which holds instances of the Ward class, representing user details.
+
+10. getWardData(): This method queries the Firestore database to get ward data based on the AuthController's w_no. It saves the data into the wardData list, which holds instances of the WardData class, representing ward details.
+
+11. The code also includes some commented-out code, which seems to be related to querying the Firestore database for specific data fields and saving them into wardProfileData and userProfileData. These commented-out sections may have been used or considered for future implementation but are currently disabled.
+
+Overall, this Dart code works as a controller to handle data retrieval and management related to user profiles and ward details from a Firebase Firestore database, making it accessible for use in a Flutter application.
+ */
